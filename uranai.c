10f9b74cfb414703738cda4d6614acf20cb1fce7
y-kato <yuki@kato.click>
2018-07-02 03:19:35 +0900

    誕生日を判定する機能を追加

--- uranai.c
+++ uranai.c
@@ -2,16 +2,35 @@
 #include<stdlib.h>
 
 int main(){
-    int birth,birth2,sum,ans;
+    int birth[2];
+    int date, month;
+    int sum,ans;
 
-    printf("Input your birthday\n");
-    printf("ex:1965-8-15==19650815>>");
-    scanf("%d",&birth);
+    int i = 0;
+    while(i < 2){
+        switch(i) {
+            case 0:
+                printf("Input your birthday\n");
+                printf("e.g. : 1965-8-15==19650815 >> ");
+                break;
+            case 1:
+                printf("\n");
+                printf("Input your partner's birthday >> ");
+        }
+        
+        scanf("%d", &birth[i]);
+        
+        date = birth[i] % 100;
+        month = ((birth[i] % 10000) - date) / 100;
+        
+        if (date <= 31 && month <= 12) {
+            i++;
+        } else {
+            printf("\n\nPlease check the input.\n\n");
+        }
+    }
 
-    printf("Input your partner's birthday>>");
-    scanf("%d",&birth2);
-
-    sum=birth+birth2;
+    sum = birth[0] + birth[1];
     sum/=256;/*キリがいいので*/
     srand(sum);
     ans=rand()%100;/*判定厳しめ*/
